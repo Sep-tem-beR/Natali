908d87e0292f353da70710da1cdf8c71cb386a09
Еvgenij Pushkaryov <rosrys2013@mail.ru>
2019-12-26 13:47:30 +0300

    tune owl script, add 'next' and 'prev' button script

--- src/NataliProgect.js
+++ src/NataliProgect.js
@@ -1,6 +1,31 @@
 import './NataliProgect.scss'
 import './owl/owl.carousel.css'
 import './owl/owl.theme.default.css'
-import './owl/owl.carousel.min'
-import './owl/animate.css'
-import './owl/owl.theme.green.css'
+import './js/owl.carousel'
+import './css/all.css'
+import './js/all'
+import './css/animate.css'
+
+$(document).ready(function(){
+    var owl=$(".owl-carousel");
+    owl.owlCarousel({
+        items: 1,
+        margin: 2,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 10000,
+        navigation : true,
+        singleItem : true,
+        animateIn: 'fadeIn',
+        nav: true,
+        navText: ['','']
+    });
+
+    $(".next").click(function(){
+		owl.trigger("next.owl.carousel");
+	});
+	$(".prev").click(function(){
+		owl.trigger("prev.owl.carousel");
+	});
+}); 
